7ef7f7a4e6ba251bcb7625988f9f3077fde8e69d
Viviana Gonzalez <vivigz06@gmail.com>
2025-06-21 17:45:54 -0300

    Prueba de funciones nombradas

--- mjs/app.mjs
+++ mjs/app.mjs
@@ -1,6 +1,14 @@
 //import por defecto
 // import sumar from "./funcionesPorDefecto.mjs"
 import mensaje from "./funcionesPorDefecto.mjs"
+//import funcion nombrada
+import {sumar,restar,mensajes} from "./funcionesNombradas.mjs"
+
+console.log(sumar(4,4));
+
+console.log(restar(4,4));
+
+console.log(mensajes());
 
 // console.log(sumar(4,4));
 console.log(mensaje("Viviii"));
---
+++ mjs/funcionesNombradas.mjs
@@ -0,0 +1,12 @@
+const sumar = (n1, n2) => {
+    return n1+n2;
+};
+
+const restar = (n1,n2)=> {
+    return n1-n2;
+};
+
+const mensajes= ()=>{
+    return `Prueba funciones nombradas`
+};
+export {sumar,restar,mensajes}
